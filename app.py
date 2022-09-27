7fadf64e9953ee92a5d9b3c92360fbf57f097841
BrockWeekley <48456042+BrockWeekley@users.noreply.github.com>
2022-09-26 22:33:04 -0500

    naming modifications

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
 from flask import Flask, request
+from flask_cors import CORS
 from TikTokApi import TikTokApi
 import glob
 import os
+import time
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='videos', static_url_path='/videos')
+cors = CORS(app)
 tiktok = TikTokApi()
 
 
 @app.route("/api/v1/videos", methods=['GET'])
 def return_videos():
+    response = {
+        "urls": []
+    }
     args = request.args
     count = int(args.get("count", 1))
     offset = int(args.get("offset", 0))
 
     i = 0
-    urls = []
     for video in tiktok.hashtag(name=args.get("tag")).videos(count, offset):
         video_bytes = video.bytes()
-        urls.append(args.get("tag") + str(i) + ".mp4")
-        with open("videos/" + args.get("tag") + str(i) + ".mp4", "wb") as out:
+        timestamp = str(round(time.time()))
+        response.get("urls").append(args.get("tag") + timestamp + ".mp4")
+        with open("videos/" + args.get("tag") + timestamp + ".mp4", "wb") as out:
             out.write(video_bytes)
         i += 1
         if i > count:
             break
-    return urls
+    return response
 
 
 @app.route("/api/v1/videos", methods=['DELETE'])
